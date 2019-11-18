9f9c5d890999f674040c1b7038c6583b19176fb2
abdullaanas <abdullaanas@bitbucket.org>
2019-11-18 16:15:18 +0530

    New APIs added

--- build/lib/snowbank/__init__.py
+++ build/lib/snowbank/__init__.py
@@ -2,7 +2,7 @@ import requests
 
 
 def initUser(token):
-    url = "https://snowbank.me:8000/init-user"
+    url = "https://api.snowbank.me/init-user"
     payload = ""
     headers = {
     'Authorization': "%s"%(token),
@@ -12,7 +12,18 @@ def initUser(token):
     return str(response.text)
 
 def createUser(password,userHash,token):
-    url = "https://snowbank.me:8000/create-user"
+    url = "https://api.snowbank.me/create-user"
+
+    payload = "{ \"password\": '%s', \"userHash\": '%s' }"%(password,userHash)
+    headers = {
+    'Content-Type': "application/json",
+    'Authorization': "%s"%(token),
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.text
+
+def unlockUser(password,userHash,token):
+    url = "https://api.snowbank.me/user/unlock"
 
     payload = "{ \"password\": '%s', \"userHash\": '%s' }"%(password,userHash)
     headers = {
@@ -23,7 +34,7 @@ def createUser(password,userHash,token):
     return response.text
 
 def userBalance(userHash,token):
-    url = "https://snowbank.me:8000/user/balance"
+    url = "https://api.snowbank.me/user/balance"
     payload = "{\"userHash\":'%s' }"%(userHash)
     headers = {
     'Authorization': "%s"%(token),
@@ -33,7 +44,7 @@ def userBalance(userHash,token):
     return response.text
 
 def generateAddress(userHash,token):
-    url = "https://snowbank.me:8000/user/generate-address"
+    url = "https://api.snowbank.me/user/generate-address"
     payload = "{\"userHash\": '%s'}"%(userHash)
     headers = {
     'Authorization': "%s"%(token),
@@ -43,7 +54,7 @@ def generateAddress(userHash,token):
     return response.text
 
 def publicKey(userHash,token):
-    url = "https://snowbank.me:8000/user/public-key"
+    url = "https://api.snowbank.me/user/public-key"
     payload = "{\"userHash\": '%s'}"%(userHash)
     headers = {
     'Authorization': "%s"%(token),
@@ -53,7 +64,7 @@ def publicKey(userHash,token):
     return response.text
 
 def getUsers(token):
-    url = "https://snowbank:8000/get-users"
+    url = "https://api.snowbank.me/get-users"
     payload = ""
     headers = {
     'Authorization': "%s"%(token),
@@ -63,7 +74,7 @@ def getUsers(token):
     return response.text
 
 def createChannel(fromUserHash,toUserHash,satoshi,token):
-    url = "https://snowbank.me:8000/create-channel"
+    url = "https://api.snowbank.me/create-channel"
     payload = "{\"fromUserHash\": '%s', \"toUserHash\": '%s', \"satoshi\": '%s'}"%(fromUserHash,toUserHash,satoshi)
     headers = {
     'Authorization': "%s"%(token),
@@ -73,11 +84,42 @@ def createChannel(fromUserHash,toUserHash,satoshi,token):
     return response.text
 
 def sendCoin(fromUserHash,toUserHash,satoshi,token):
-    url = "https://snowbank.me:8000/send-coin"
+    url = "https://api.snowbank.me/send-coin"
     payload = "{\"fromUserHash\": '%s' , \"toUserHash\": '%s', \"satoshi\": '%s'}"%s(fromUserHash,toUserHash,satoshi)
     headers = {
     'Authorization': "%s"%(token),
     'Content-Type': "application/json"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.text
+
+def deleteChannel(fromUserHash,toUserHash,token):
+    url = "https://api.snowbank.me/delete-channel"
+    payload = "{\"fromUserHash\": '%s', \"toUserHash\": '%s'}"%(fromUserHash,toUserHash)
+    headers = {
+    'Authorization': "%s"%(token),
+    'Content-Type': "application/json"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.text
+
+def deletePeer(fromUserHash,toUserHash,token):
+    url = "https://api.snowbank.me/delete-peer"
+    payload = "{\"fromUserHash\": '%s', \"toUserHash\": '%s'}"%(fromUserHash,toUserHash)
+    headers = {
+    'Authorization': "%s"%(token),
+    'Content-Type': "application/json"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.text
+
+def changePassword(currentPassword,newPassword,userHash,token):
+    url = "https://api.snowbank.me/create-user"
+
+    payload = "{ \"currentPassword\": '%s', \"newPassword\": '%s', \"userHash\": '%s' }"%(currentPassword,newPassword,userHash)
+    headers = {
+    'Content-Type': "application/json",
+    'Authorization': "%s"%(token),
     }
     response = requests.request("POST", url, data=payload, headers=headers)
     return response.text
